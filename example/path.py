2a7ce52a2c2c93691d6dc3f1b79df8a39aa2f094
m-chardelin <marialine.chardelin@laposte.net>
2023-05-05 18:30:49 +0200

    fix repo tablet

--- example/path.py
+++
@@ -1,9 +0,0 @@
-folder = '/Users/marialinechardelin/current'
-#folder = '/storage/emulated/0/toDo'
-
-
-treatment = f'{folder}/ZabargadToDo/3_4_deformation/3_treatment/EBSD'
-analysis = f'{folder}/ZabargadToDo/3_4_deformation/4_analysis'
-
-dataClean = f'{folder}/ZabargadToDo/3_4_deformation/3_treatment/EBSD/dataClean'
-tasks = f'{folder}/ZabargadToDo/3_4_deformation/3_treatment/EBSD/output/tasks'
